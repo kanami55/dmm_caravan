0a9fdea386f5047de2907fd2f804d0699c97c433
Higashimoto Kanami <summerwave5959@gmail.com>
2022-06-10 08:52:30 +0900

    [Fix] index.html修正

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-const app = new Vue ({
+var app = new Vue ({
     el: '#app',
     data: {
         open: false
+    },
+    method: {
+        scrollTop: function() {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+        }
     }
+
 })
+
