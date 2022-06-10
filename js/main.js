482937092dc08d5a9067f5695dd0d6a947cac4d5
Higashimoto Kanami <summerwave5959@gmail.com>
2022-06-10 09:51:30 +0900

    [Add] スクロールしたらボタン表示

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const app = new Vue ({
     el: '#app',
     data: {
-        open: false
+        open: false,
+        buttonActive: false,//ボタンを非表示にしておく
+        scroll: 0
+    },
+    mounted() {
+        window.addEventListener('scroll',this.scrollWindow)
     },
     methods: {
         pageTop() {
@@ -9,7 +14,16 @@ const app = new Vue ({
                 top: 0,
                 behavior: 'smooth'
             })
-        }  
+        },
+        scrollWindow() {
+            const top = 100
+            this.scroll = window.scrollY //垂直方向
+            if (top <= this.scroll) {
+                this.buttonActive = true
+            } else {
+                this.buttonActive = false
+            }
+        } 
     }
 })
 
