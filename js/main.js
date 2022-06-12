c7ae8911870aee97b791520379c2fd485551bf1c
Higashimoto Kanami <summerwave5959@gmail.com>
2022-06-12 09:18:07 +0900

    [Add] お問い合わせフォームVue.js追加

--- js/main.js
+++ js/main.js
@@ -3,7 +3,22 @@ const app = new Vue ({
     data: {
         open: false,
         buttonActive: false,//ボタンを非表示にしておく
-        scroll: 0
+        scroll: 0,
+        name: '',
+        phone: '',
+        email: '',
+    },
+    computed: { //関数定義
+        isValidName() {
+            return this.name.length < 2
+        },
+        isValidPhone() {
+            return this.phone.length < 8
+        },
+        isValidEmail() {
+            const regex = new RegExp(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i)
+            return !regex.test(this.email);
+        }
     },
     mounted() {
         window.addEventListener('scroll',this.scrollWindow)
